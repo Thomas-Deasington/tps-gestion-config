ec6f537391498669f1bc5c3a94885221c1de249b
Thomas-Deasington <t.deasington@gmail.com>
2022-12-15 12:45:21 +0100

    tp1: add ansible dir

--- tp1/vagrant/setup.sh
+++ tp1/vagrant/setup.sh
@@ -9,3 +9,7 @@ dnf install -y ansible
 # désactive SELinux
 setenforce 0
 sed -i 's/SELINUX=enforcing/SELINUX=permissive/g' /etc/selinux/config
+
+# Create an user that has access to root rights
+useradd -m -s /bin/bash vagrant
+echo vagrant ALL=NOPASSWD:ALL > /etc/sudoers.d/vagrant
